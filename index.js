556ddaafb9bc6f0a908c28745d10d36fd87b11bb
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-09-29 21:17:58 +0530

    third commit

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(cors({
     origin:process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials:true
 }));
 app.use(cookieParser());
 
