20ade5404a52d0a86e4eda3bf1e082aa8b2d7c2b
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-09-30 12:16:34 +0530

    final

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin:process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials:true
+    credentials:true,
+    secure: true, 
+    sameSite: 'None',
 }));
 
 app.use('/auth',authRoutes);
