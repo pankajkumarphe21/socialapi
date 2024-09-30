4962353c8c873c831810b09209dca4f7e1276d70
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-09-30 12:08:56 +0530

    h

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import cookieParser from "cookie-parser";
 const app=express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     origin:process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials:true
 }));
-app.use(cookieParser());
 
 app.use('/auth',authRoutes);
 app.use('/post',postRoutes);
