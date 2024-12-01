f722f0611d3cf4a078449ddcd7e13aedcfbbf61e
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-12-01 14:02:57 +0530

    some mixing solved

--- routes/post.js
+++ routes/post.js
@@ -6,11 +6,11 @@ import upload from '../config/multer.js'
 const router=express.Router();
 
 router.post('/create',isLoggedIn,upload.single('image'),createPost);
+router.get('/find',isLoggedIn,getPosts);
 router.get('/find/:userId',isLoggedIn,getUserPosts);
 router.get('/:postId',isLoggedIn,getPost);
 router.post('/edit/:postId',isLoggedIn,upload.single('image'),editPost);
 router.post('/like/:postId',isLoggedIn,likePost);
-router.get('/find',isLoggedIn,getPosts);
 router.delete('/delete/:postId',isLoggedIn,deletePosts);
 
 export default router;
